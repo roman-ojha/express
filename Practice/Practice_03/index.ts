f64bceb124269b4a7528481aecfb4294264d889b
Roman-Ojha <razzroman98@gmail.com>
2023-07-25 20:25:31 +0545

    feat(practice 03): rendered html view from hbs view engine

--- Practice/Practice_03/index.ts
+++ Practice/Practice_03/index.ts
@@ -1,11 +1,20 @@
-const express = require("express");
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const staticPath = path.join(__dirname, "/public");
 
-app.get("/",(req, res)=>{
-  res.send("Hello world");
+app.use(express.static(staticPath));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "/view"));
+
+app.get("/", (req, res) => {
+  res.render("index", {});
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
