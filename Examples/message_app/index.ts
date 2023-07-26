28eee8cc9d3d7c7b43faf5bd22c678c081462b09
Roman-Ojha <razzroman98@gmail.com>
2023-07-26 21:51:20 +0545

    feat(message_app): configured database connection

--- Examples/message_app/index.ts
+++ Examples/message_app/index.ts
@@ -1,20 +1,38 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import db from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const staticPath = path.join(__dirname, "/public");
 
-app.use(express.static(staticPath));
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "/view"));
+(() => {
+  try {
+    db.connect((err) => {
+      if (err) {
+        console.error("Database connection error: " + err.stack);
+        return;
+      }
 
-app.get("/", (req, res) => {
-  res.render("index", {});
-});
+      // console.log("Database connected as id " + db.threadId);
+      console.log("Database connected");
+    });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    app.use(express.static(staticPath));
+    app.set("view engine", "hbs");
+    app.set("views", path.join(__dirname, "/view"));
+
+    app.get("/", (req, res) => {
+      res.render("index", {});
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+    db.end();
+  } catch (err) {
+    console.log(err);
+  }
+})();
